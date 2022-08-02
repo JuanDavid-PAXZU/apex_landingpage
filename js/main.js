6dbc9bce4a40e2468ae3f402ab15901a558987db
Juan David <juan.reyes@paxzu.co>
2022-08-02 17:11:30 -0500

    ajuste modal y efectos

--- js/main.js
+++ js/main.js
@@ -33,146 +33,86 @@ $(document).ready(function(){
             }
         }
     });
+
+    /* FUNCION MODAL FORMULARIO */
+
+    $('#formModal').click(function(){
+        $('.modal_form').fadeToggle();
+        $('body').css('overflow', 'hidden');
+    })
     
-    function dust(){
-        /* REVISED DUST EFFECT POC */
+    $('.dark_bg').click(function(){
+        $('.modal_form').fadeOut();
+        $('body').css('overflow', 'scroll');
+    })
 
-        let canvas = document.getElementById('canvas');
-        let ctx = canvas.getContext('2d');
-        let width = window.innerWidth;
-        let height = window.innerHeight;
+    /* FIN MODAL FORMULARIO */
 
-        class Particle {
-        constructor() {
-            this.x = 0;
-            this.y = 0;
-            this.vx = 0;
-            this.vy = 0;
-            this.accelX = 0;
-            this.accelY = 0;
-            this.life = 2000;
-            this.alpha = 1;
-            this.size = 2;
-        }
-        
-        update() {
-            this.vx += this.accelX;
-            this.vy += this.accelY;
-            this.x += this.vx;
-            this.y += this.vy;
-        }
-        
-        draw(ctx) {
-            ctx.fillStyle = `rgba(34, 162, 238, ${this.alpha})`;
-            ctx.beginPath();
-            ctx.arc(this.x, this.y, this.size, 0, Math.PI*2);
-            ctx.fill();
-            ctx.closePath();
-        }
-        
-        isAlive() {
-            return this.life >= 0;
-        }
-        }
+    
 
-        class ParticleSystem {
-        constructor() {
-            this.particles = [];
-            this.updateHandler = undefined;
-        }
-        
-        addParticle(particle) {
-            this.particles.push(particle);
-        }
+    function mail(){
 
-        update(deltaTime = 0) {
-            this.particles.forEach(particle => {
-            particle.update(deltaTime);
-            this.updateHandler && this.updateHandler(particle);
-            });
-        }
-        
-        onUpdate(fn) {
-            this.updateHandler = fn;
-        }
-        }
 
-        let system = new ParticleSystem();
+        let line1 = $('#env-line-1');
+        let line2 = $('#env-line-2');
+        let line3 = $('#env-line-3');
+        let mailIcon = $('#submit_form');
+        let envLid = $('#env-lid');
+        let envPaper = $('#env-paper');
 
-        for (let i = 0; i < 200; i++) {
-        let particle = new Particle();
-        let angle = Math.PI/2 * (Math.random() - 0.5) * 2;
-        particle.x = Math.random() * width;
-        particle.y = Math.random() * height;
-        particle.life = Math.random() * 1000 + 1000;
-        particle.size = Math.random() * 1.5;  
-        particle.maxLife = particle.life;
-        system.addParticle(particle);
-        }
 
-        system.onUpdate((particle) => {
-        if (!particle.isAlive()) {
-            particle.x = Math.random() * width;
-            particle.y = Math.random() * height;
-            particle.vx = 0;
-            particle.vy = 0;
-            particle.life = Math.random() * 1000 + 1000;
-            particle.maxLife = particle.life;    
-        }
-        
-        particle.life -= 10;
-        particle.accelX = (Math.random() - 0.5) * 0.02;
-        particle.accelY = (Math.random() - 0.5) * 0.02;
 
-        if (particle.life >= particle.maxLife / 2) {
-            particle.alpha = 1 - (particle.life / particle.maxLife);
-        } else {
-            particle.alpha = particle.life / particle.maxLife;
-        }
-
-        particle.update();
+        let tl = new TimelineLite({
+            paused:true,
         });
 
-        function update() {
-        system.update();
-        }
-
-        function draw() {
-        ctx.globalCompositeOperation = 'source-over';
-        ctx.fillStyle = '#010101';
-        ctx.fillRect(0, 0, width, height);
+        TweenLite.defaultEase = Back.easeOut;
         
-        let grad = ctx.createRadialGradient(width / 2, height / 2, 0, width / 2, height/2, width);
-        grad.addColorStop(0, '#115a97');
-        grad.addColorStop(0.75, 'transparent');
-        
-        ctx.fillStyle = grad;
-        ctx.fillRect(0, 0, width, height);
-        
-        ctx.globalCompositeOperation = 'lighter';
-        ctx.fillStyle = '#22a2ee';
-        
-        system.particles.forEach(particle => particle.draw(ctx));
-        }
+        tl 
+            .to(envLid, 0.3, {
+                scaleY:-1,
+                y: 1.5,
+                }
+            )
+            .fromTo(envPaper, 0.4, {
+                transformOrigin: "50% 100%",
+                scaleY:0,
+            },{
+                scaleY: 1,
+            }, "=-0.25")
+            .staggerFromTo([line1, line2, line3], 0.3, {
+                transformOrigin: "50% 50%",
+                scaleX: 0
+            },{
+                scaleX: 1,
+            },	-0.09)
 
-        function render() {
-        update();
-        draw();
-        requestAnimationFrame(render);
-        }
 
-        function setup() {
-        canvas.width = width;
-        canvas.height = height;
-        }
 
-        function init() {
-        setup();
-        render();
-        }
+        $(mailIcon).click(function(){
+            
+            if ( $(this).hasClass('toggled') ) {
+                /* tl.reverse(); */
+            } 
+            
+            else {
+                tl.play();
+            }
+            
+            $(this).toggleClass('toggled');
+            
+        });
 
-        init();
     }
 
-    dust();
+
+    mail();
+
+
+    /* prevent default button form */
+    
+    document.getElementById("submit_form").addEventListener("click", function(event){
+        event.preventDefault()
+    });
+
 })
